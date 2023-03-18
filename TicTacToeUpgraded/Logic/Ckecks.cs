e3d384bef35bc118874e469263b33b3c3de8c5c4
Rolincero <ms.mayron29@gmail.com>
2023-03-18 15:41:45 +1000

    Methods for cheking some game events.

---
+++ TicTacToeUpgraded/Logic/Ckecks.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTacToeUpgraded.Const;
+
+namespace TicTacToeUpgraded.Logic
+{
+    public class Ckecks
+    {
+        public static bool BoardIsFull()
+        {
+            foreach (var item in Board.board)
+            {
+                if (item == '.')
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CheckPos(int x, int y)
+        {
+            try
+            {
+                if (Board.board[x, y] == Signs.Player || Board.board[x, y] == Signs.Ai)
+                {
+                    return false;
+                }
+                else if (Board.board[x, y] == Signs.Empty)
+                {
+                    return true;
+                }
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
